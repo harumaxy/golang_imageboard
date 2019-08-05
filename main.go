7c88072c4cc620b49152119dad36091a3ef03a8c
max <harumaxy@gmail.com>
2019-08-05 11:08:08 +0900

    postsに対する操作をurlグループでまとめてrouterに記述

--- main.go
+++ main.go
@@ -2,9 +2,19 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/golang_imageboard/handlers"
 )
 
 func main() {
 	r := gin.Default()
+	posts := r.Group("/posts")
+	{
+		pc := handlers.PostController{}
+		posts.GET("", pc.List())
+		posts.POST("", pc.Create())
+		posts.GET("/:id", pc.Read())
+		posts.PUT("/:id", pc.Update())
+		posts.DELETE("/:id", pc.Delete())
+	}
 	r.Run()
 }
