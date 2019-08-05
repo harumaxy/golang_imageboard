8d7d58bddb8e20b8af8ee31e5d1c1960e7ea5822
max <harumaxy@gmail.com>
2019-08-05 17:46:48 +0900

    Createページへのルーティングを追加

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route, Switch, Link} from "react-router-dom"
 import * as serviceWorker from './serviceWorker';
 
 import List from "./components/List"
+import Create from "./components/Create"
 
 
-ReactDOM.render(<List />, document.getElementById('root'));
+ReactDOM.render(
+    <React.Fragment>
+        <Router>
+            <Switch>
+                <Route path="/posts/create" component={Create}/>
+                <Route path="" component={List}/>
+            </Switch>
+            <p><Link to="">List</Link></p>
+            <p><Link to="/posts/create">Create</Link></p>
+        </Router>
+    </React.Fragment>
+
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
