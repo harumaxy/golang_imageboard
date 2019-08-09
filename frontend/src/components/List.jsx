8ccd2934b232fbce7f4d406a94c0a221f00b601b
max <harumaxy@gmail.com>
2019-08-09 13:05:46 +0900

    投稿のリスト表示を実装

--- frontend/src/components/List.jsx
+++ frontend/src/components/List.jsx
@@ -1,26 +1,82 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { PostContainer } from "../containers/PostContainer"
 import _ from "lodash"
+import { Paper, Card, CardActionArea, CardMedia, CardContent, Typography, CardActions, Button, Container, GridList, GridListTile, ListSubheader, GridListTileBar, IconButton } from "@material-ui/core";
+
+import InfoIcon from "@material-ui/icons/Info";
+
+import dummy_img from './dummy.jpeg'
 
 const List = () => {
     const { posts, fetch_posts } = PostContainer.useContainer()
+    useEffect(() => {
+        fetch_posts()
+    }, [])
     return (
-        <React.Fragment>
-            {_.map(posts, (post) => {
-                return (
-                    <div>
-                        <p>{post.ID}</p>
-                        <p>{post.title}</p>
-                        <p>{post.author}</p>
-                        <p>{post.description}</p>
-                        <p>{post.image_src}</p>
-                    </div>
-                )
-            })}
-            <button onClick={() => {fetch_posts()}}>Fetch Posts</button>
-        </React.Fragment>
+        LayoutCard(posts)
     )
 }
+
+const LayoutGrid = (posts) => {
+    return (
+        <Paper>
+            <GridList cellHeight={300} cols={3}>
+                <GridListTile key="SubHeader" style={{ height: "auto" }} cols={3}>
+                    <ListSubheader component="div">Posts</ListSubheader>
+                </GridListTile>
+
+                {_.map(posts, (post) => (
+                    <GridListTile key={post.image_src}>
+                        <img src={post.image_src} alt={post.title} />
+                        <GridListTileBar
+                            title={post.title}
+                            subtitle={<span>by: {post.author}</span>}
+                            actionIcon={
+                                <IconButton aria-label={`info about ${post.title}`} style={{ color: "white" }}>
+                                    <InfoIcon />
+                                </IconButton>
+                            }
+                        />
+                    </GridListTile>
+                ))}
+            </GridList>
+        </Paper>
+    )
+}
+
+
+// Cardスタイル
+const LayoutCard = (posts) => {
+    return (
+        <Paper>
+            <GridList cols={3} cellHeight="auto">
+                <GridListTile key="SubHeader" cols={3}>
+                    <ListSubheader component="div">Posts</ListSubheader>
+                </GridListTile>
+
+                {_.map(posts, (post)=>(
+                    <GridListTile cols={1}>
+                        {renderCard(post)}
+                    </GridListTile>
+                ))}
+            </GridList>
+        </Paper>
+    )
+}
+
+const renderCard = (post)=>(
+    <Card>
+        <CardContent>
+            <CardMedia image={dummy_img} component="img" alt={post.title}/>
+            <Typography gutterBottom variant="h5" component="h2">
+                {post.title}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+                {post.description || "nothing"}
+            </Typography>
+        </CardContent>
+    </Card>
+)
 
 export default () => {
     return (
