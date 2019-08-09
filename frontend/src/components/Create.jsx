fb6beecb0364a97e9c3f2320da08fb6ab6d91583
max <harumaxy@gmail.com>
2019-08-09 13:58:11 +0900

    クラスをラップするとHistory APIがうまく使えないのでラップしない形に修正

--- frontend/src/components/Create.jsx
+++ frontend/src/components/Create.jsx
@@ -19,6 +19,11 @@ class CreateForm extends React.Component {
         }
     }
 
+    constructor(props) {
+        super(props)
+        this.history = props.history
+    }
+
 
     handleSubmit = async (event) => {
         event.preventDefault()
@@ -30,7 +35,7 @@ class CreateForm extends React.Component {
             description: formData.description,
             image_src: formData.image_src
         })
-        this.props.history.push(`${API_ROOT}/posts`)
+        this.history.push(`${API_ROOT}/posts`)
 
     }
     handleChange = (event) => {
@@ -42,69 +47,63 @@ class CreateForm extends React.Component {
     render = () => {
         const { formData, submitted } = this.state
         return (
-            <form onSubmit={this.handleSubmit} autoComplete="off" style={{margin: 20, width: "100%"}}>
+            <Paper>
+                <Grid container justify="flex-start">
+                    <form onSubmit={this.handleSubmit} autoComplete="off" style={{ margin: 20, width: "100%" }}>
 
 
-                <TextField
-                    required
-                    label="Title"
-                    name="title"
-                    onChange={this.handleChange}
-                    value={this.state.formData.title}
-                    variant="outlined"
-                />
-                <br />
-                <br />
-                
+                        <TextField
+                            required
+                            label="Title"
+                            name="title"
+                            onChange={this.handleChange}
+                            value={this.state.formData.title}
+                            variant="outlined"
+                        />
+                        <br />
+                        <br />
 
-                <TextField
-                    label="Author"
-                    name="author"
-                    onChange={this.handleChange}
-                    value={this.state.formData.author}
-                    variant="outlined"
-                />
-                <br />
-                <br />
-                <TextField
-                    label="Description"
-                    name="description"
-                    onChange={this.handleChange}
-                    value={this.state.formData.description}
-                    multiline
-                    rows="4"
-                    fullWidth
-                    variant="outlined"
-                />
-                <br />
-                <br />
-                <TextField
-                    label="image_src"
-                    name="image_src"
-                    onChange={this.handleChange}
-                    value={this.state.formData.image_src}
-                    variant="outlined"
-                    fullWidth
-                />
-                <br />
-                <br />
-                <Button type="submit" variant="contained" color="primary" disabled={submitted}>
-                    Submit
+
+                        <TextField
+                            label="Author"
+                            name="author"
+                            onChange={this.handleChange}
+                            value={this.state.formData.author}
+                            variant="outlined"
+                        />
+                        <br />
+                        <br />
+                        <TextField
+                            label="Description"
+                            name="description"
+                            onChange={this.handleChange}
+                            value={this.state.formData.description}
+                            multiline
+                            rows="4"
+                            fullWidth
+                            variant="outlined"
+                        />
+                        <br />
+                        <br />
+                        <TextField
+                            label="image_src"
+                            name="image_src"
+                            onChange={this.handleChange}
+                            value={this.state.formData.image_src}
+                            variant="outlined"
+                            fullWidth
+                        />
+                        <br />
+                        <br />
+                        <Button type="submit" variant="contained" color="primary" disabled={submitted}>
+                            Submit
                 </Button>
-            </form>
+                    </form>
+                </Grid>
+            </Paper>
         )
     }
 
 }
 
-const layoutWrap = () => {
-    return (
-        <Paper>
-            <Grid container justify="flex-start">
-                <CreateForm />
-            </Grid>
-        </Paper>
-    )
-}
-
-export default layoutWrap
+export default CreateForm
