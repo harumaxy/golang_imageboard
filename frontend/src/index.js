3c548bed62bf558e0b1697b3ad0b62172e3c74cd
max <harumaxy@gmail.com>
2019-08-11 19:41:58 +0900

    Routeにexactを追加、正確なpathを入力した場合のみコンポーネントを描写

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -13,10 +13,9 @@ ReactDOM.render(
     <PostContainer.Provider>
         <Router>
             <Switch>
-                <Route path="/posts/create" exact component={Create}/>
-                <Route path="" exact component={List}/>
-
-                <Route path="/posts/:id" component={Read}/>
+                <Route exact path="/posts/create" exact component={Create}/>
+                <Route exact path="/posts/:id" component={Read}/>
+                <Route exact path="" exact component={List}/>
             </Switch>
             <p><Link to="">List</Link></p>
             <p><Link to="/posts/create">Create</Link></p>
