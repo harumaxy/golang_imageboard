0ce22797659e5ed0d7a9003af6b7f31f9ccf850c
max <greatsword8821@gmail.com>
2019-10-03 18:43:06 +0900

    コメント表示窓を追加

--- frontend/src/components/Read.tsx
+++ frontend/src/components/Read.tsx
@@ -1,19 +1,22 @@
-import React, {useState, useEffect, ReactNode} from "react"
-import { Container, Grid , Typography, Hidden, Paper} from "@material-ui/core";
+import React, { useState, useEffect, ReactNode } from "react"
+import { Container, Grid, Typography, Hidden, Paper } from "@material-ui/core";
 import { PostContainer } from "../containers/PostContainer"
 import { string } from "prop-types";
 import Axios, { AxiosResponse } from "axios";
-import {API_ROOT} from '../setting'
+import { API_ROOT } from '../setting'
 
 
 type Post = {
     ID: number,
     CreatedAt: string,
     title: string,
-	author: string,
-	description: string,
-	image_src: string,
-	comments: {}[]
+    author: string,
+    description: string,
+    image_src: string,
+
+    // UpdatedAt: string,
+    // DeletedAt: string,
+    // comments: []
 }
 
 const Read = (props: any) => {
@@ -36,49 +39,78 @@ const Read = (props: any) => {
 
     return (
         <Paper>
-        <Container>
-            <img src={post.image_src} alt={post.title} width="100%"/>
-            <Typography variant="h4" component="h1">{post.title}</Typography>
-            <Typography variant="subtitle1" component="h2">{post.author}</Typography>
-            <Typography variant="body1" component="p">{post.description}</Typography>
-            <Typography variant="srOnly">{post.image_src}</Typography>
-            <Typography variant="srOnly">{id}</Typography>
-        </Container>
+            <Container>
+                <img src={post.image_src} alt={post.title} width="100%" />
+                <Typography variant="h4" component="h1">{post.title}</Typography>
+                <Typography variant="subtitle1" component="h2">{post.author}</Typography>
+                <Typography variant="body1" component="p">{post.description}</Typography>
+                <Typography variant="srOnly">{post.image_src}</Typography>
+                <Typography variant="srOnly">{id}</Typography>
+            </Container>
+            <Container>
+                <CommentList post_id={id} />
+            </Container>
         </Paper>
     )
 }
 
 const CommentForm = () => {
-    
+
 }
 
 const fetchComments = async (post_id: number) => {
-    
+
 }
 
 type Comment = {
     ID: number,
     CreatedAt: string,
+    author: string,
+    body: string,
 
-
+    // 使わないフィールド
+    // post_id: number,
+    // UpdatedAt: string,
+    // DeletedAt: string,
 }
 type CommentListProps = {
     post_id: number,
-    CreatedAt: string,
 }
 const CommentList: React.FC<CommentListProps> = (props: CommentListProps) => {
-    const [comments, setComments] = useState([]);
+    const [comments, setComments] = useState([] as Comment[]);
     const { post_id } = props
     useEffect(() => {
-        Axios.get(`${API_ROOT}/${post_id}/comments`).then(
-            (res: AxiosResponse) => {setComments(res.data)}
+        Axios.get(`${API_ROOT}/posts/${post_id}/comments`).then(
+            (res: AxiosResponse) => { setComments(res.data) }
         )
     }, [])
-    
-    return(
-        <>
-        
 
+    return (
+        <>
+            <br/>
+            <hr />
+            <h3>Comments</h3>
+            <div style={{ overflow: "scroll", height: 300, border: "solid 1px gray" }}>
+
+                {comments.map((com, index) => (
+                    <>
+                    <Grid container alignItems="center" style={{margin: 10}}>
+                        <Grid item xs={6}>
+                            <Typography variant="h6">{com.ID}: {com.author}</Typography>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Typography variant="body2">{com.CreatedAt}</Typography>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <p>{com.body}</p>
+                        </Grid>
+                        
+                    </Grid>
+                    <hr/>
+                    </>
+                ))}
+
+            </div>
         </>
     )
 }
