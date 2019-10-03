87c90c45975932f94bbed8e831846747fecdbc01
max <greatsword8821@gmail.com>
2019-10-03 14:14:22 +0900

    .envで設定を読み込むとdocker-composeと競合するので、yamlファイル形式に変更した

--- auth/middleware.go
+++ auth/middleware.go
@@ -4,15 +4,21 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
+
+	"gopkg.in/yaml.v2"
 
 	jwtmiddleware "github.com/auth0/go-jwt-middleware"
 	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
 )
+
+type Auth0Setting struct {
+	AUTH0_CLIENT_ID string `yaml:"AUTH0_CLIENT_ID"`
+	AUTH0_DOMAIN    string `yaml:"AUTH0_DOMAIN"`
+}
 
 type Jwks struct {
 	Keys []JSONWebKeys `json:"keys"`
@@ -30,12 +36,13 @@ type JSONWebKeys struct {
 
 var (
 	globalJwtMiddleWare *jwtmiddleware.JWTMiddleware
+	settings            Auth0Setting
 )
 
 // 公開鍵を取得する
 func getPemCert(token *jwt.Token) (string, error) {
 	cert := ""
-	resp, err := http.Get(os.Getenv("AUTH0_DOMAIN") + ".well-known/jwks.json")
+	resp, err := http.Get(settings.AUTH0_DOMAIN + ".well-known/jwks.json")
 	if err != nil {
 		return cert, err
 	}
@@ -79,19 +86,19 @@ func NewAuthMiddleware() gin.HandlerFunc {
 }
 
 func initMiddleware() {
-	// 環境変数をロードする
-	godotenv.Load()
+	initSetting()
+	// jwt認証ミドルウェアを作成
 	jwtMiddleware := jwtmiddleware.New(jwtmiddleware.Options{
 		ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
 
 			// aud を検証
-			aud := os.Getenv("AUTH0_CLIENT_ID")
+			aud := settings.AUTH0_CLIENT_ID
 			checkAudience := token.Claims.(jwt.MapClaims).VerifyAudience(aud, false)
 			if !checkAudience {
 				return token, errors.New("Invalid audience.")
 			}
 			// iss を検証
-			iss := os.Getenv("AUTH0_DOMAIN")
+			iss := settings.AUTH0_DOMAIN
 			checkIss := token.Claims.(jwt.MapClaims).VerifyIssuer(iss, false)
 			if !checkIss {
 				return token, errors.New("Invalid issuer.")
@@ -112,4 +119,14 @@ func initMiddleware() {
 	globalJwtMiddleWare = jwtMiddleware
 
 	// ... the rest of the code below this function doesn't change yet
+}
+
+// YAMLファイルに書かれたドメイン情報を読み込む
+func initSetting() {
+	// Auth0のドメインやクライアントIDをグローバル変数のsettingに読み込む
+	buf, err := ioutil.ReadFile("./setting.yml") // main.goからの相対パス
+	if err != nil {
+		panic(err)
+	}
+	err = yaml.Unmarshal(buf, &settings)
 }
