bddd8d7d6b927c62089cc79fc61c50b0f3ca6bd6
max <greatsword8821@gmail.com>
2019-10-03 21:46:51 +0900

    コメント機能を追加

--- frontend/src/components/Read.tsx
+++ frontend/src/components/Read.tsx
@@ -1,9 +1,14 @@
-import React, { useState, useEffect, ReactNode } from "react"
-import { Container, Grid, Typography, Hidden, Paper, Grow } from "@material-ui/core";
+import React, { useState, useEffect, ChangeEvent, ChangeEventHandler, FormEvent, FormEventHandler } from "react"
+import { Container, Grid, Typography, Hidden, Paper, Grow, Input, InputAdornment, Avatar } from "@material-ui/core";
+import { TextField, Button, FormControl, InputLabel } from "@material-ui/core";
+import { AccountCircle } from '@material-ui/icons';
+
 import { PostContainer } from "../containers/PostContainer"
 import { string } from "prop-types";
-import Axios, { AxiosResponse } from "axios";
+import Axios, { AxiosResponse, AxiosRequestConfig } from "axios";
 import { API_ROOT } from '../setting'
+
+import { isLoggedIn } from '../utils/isLoggedIn'
 
 
 type Post = {
@@ -20,8 +25,6 @@ type Post = {
 }
 
 const Read = (props: any) => {
-
-
     const { params } = props.match
     const id = parseInt(params.id, 10)
     const { posts, fetch_single_post } = PostContainer.useContainer()
@@ -46,7 +49,7 @@ const Read = (props: any) => {
                         <img src={post.image_src} alt={post.title} width="100%" />
                         <Typography variant="h4" component="h1">{post.title}</Typography>
                         <Typography variant="subtitle1" component="h2">{post.author}</Typography>
-                        <Typography variant="body1" component="p">{post.description}</Typography>
+                        <Typography variant="body1" component="p" style={{ whiteSpace: "pre-line" }}>{post.description}</Typography>
                         <Typography variant="srOnly">{post.image_src}</Typography>
                         <Typography variant="srOnly">{id}</Typography>
                     </Container>
@@ -55,19 +58,14 @@ const Read = (props: any) => {
                     <Container>
                         <CommentList post_id={id} />
                     </Container>
+
                 </Grid>
             </Grid>
         </Paper>
     )
 }
 
-const CommentForm = () => {
 
-}
-
-const fetchComments = async (post_id: number) => {
-
-}
 
 type Comment = {
     ID: number,
@@ -86,18 +84,22 @@ type CommentListProps = {
 const CommentList: React.FC<CommentListProps> = (props: CommentListProps) => {
     const [comments, setComments] = useState([] as Comment[]);
     const { post_id } = props
-    useEffect(() => {
+    const update = () => {
         Axios.get(`${API_ROOT}/posts/${post_id}/comments`).then(
             (res: AxiosResponse) => { setComments(res.data) }
         )
+    }
+
+    useEffect(() => {
+        update()
     }, [])
+
 
     return (
         <>
             <hr />
             <h3>Comments</h3>
             <div style={{ overflow: "scroll", height: 300, border: "solid 1px gray" }}>
-
                 {comments.map((com, index) => (
                     <>
                         <Grid container style={{ margin: 10 }}>
@@ -107,15 +109,96 @@ const CommentList: React.FC<CommentListProps> = (props: CommentListProps) => {
                             <Grid item xs={12} sm={6}>
                                 <Typography variant="body2">{com.CreatedAt}</Typography>
                             </Grid>
-                            <p>{com.body}</p>
+                            <p style={{ whiteSpace: "pre-line" }}>{com.body}</p>
                         </Grid>
                         <hr />
                     </>
                 ))}
-
             </div>
+            <Container>
+                <CommentForm post_id={post_id} update={update} />
+            </Container>
         </>
     )
 }
+
+type CommentFormProps = {
+    post_id: number,
+    update: () => void,
+}
+
+const CommentForm: React.FC<CommentFormProps> = (props: CommentFormProps) => {
+    const user_info = isLoggedIn ? (JSON.parse(localStorage.getItem("user_info") as string)) : null
+    const [author, setAuthor] = useState(user_info.name || "no name")
+    const [body, setBody] = useState("")
+
+
+    const { post_id, update } = props
+
+    const handleSubmit: FormEventHandler = (e: FormEvent) => {
+        e.preventDefault()
+        Axios.post(`${API_ROOT}/posts/${post_id}/comments`,
+            {
+                post_id: post_id,
+                author: author,
+                body: body
+            },
+            {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem("id_token")}`,
+                }
+            }
+        ).then(
+            () => { update() }
+        )
+    }
+
+    const handleChange: ChangeEventHandler = (e: ChangeEvent) => {
+        const target = e.target as HTMLInputElement
+        switch (target.name) {
+            case "author":
+                setAuthor(target.value)
+                break
+            case "body":
+                setBody(target.value)
+                break
+            default:
+                console.log("Impossible change event");
+        }
+    }
+
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <Grid container spacing={2} alignItems="center" style={{ padding: 10 }}>
+                    <Grid item xs={1}>
+                        <Avatar
+                            alt={user_info.name}
+                            srcSet={user_info.picture}
+                        />
+                    </Grid>
+                    <Grid item xs={9}>
+                        <TextField
+                            label="comment"
+                            name="body"
+                            onChange={handleChange}
+                            value={body}
+                            multiline
+                            rows="2"
+                            fullWidth
+                            variant="outlined"
+                        />
+                    </Grid>
+                    <Grid item xs={2}>
+                        <Button type="submit" variant="contained" color="primary">
+                            Submit
+                        </Button>
+                    </Grid>
+                </Grid>
+            </form>
+        </>
+    )
+}
+
 
 export default Read
