b51da0ecd8c01aa12e2d6ad9735fdedee8d0ad74
max <greatsword8821@gmail.com>
2019-10-03 18:10:52 +0900

    Readページをtsxに変更

--- frontend/src/components/Read.tsx
+++ frontend/src/components/Read.tsx
@@ -1,15 +1,29 @@
-import React from "react"
+import React, {useState, useEffect, ReactNode} from "react"
 import { Container, Grid , Typography, Hidden, Paper} from "@material-ui/core";
 import { PostContainer } from "../containers/PostContainer"
+import { string } from "prop-types";
+import Axios, { AxiosResponse } from "axios";
+import {API_ROOT} from '../setting'
 
-const Read = (props) => {
+
+type Post = {
+    ID: number,
+    CreatedAt: string,
+    title: string,
+	author: string,
+	description: string,
+	image_src: string,
+	comments: {}[]
+}
+
+const Read = (props: any) => {
 
 
     const { params } = props.match
     const id = parseInt(params.id, 10)
     const { posts, fetch_single_post } = PostContainer.useContainer()
     console.log(posts);
-    const post = posts[id]
+    const post = (posts as Array<Post>)[id]
     if (post === undefined) {
         fetch_single_post(id)
         return (
@@ -31,6 +45,41 @@ const Read = (props) => {
             <Typography variant="srOnly">{id}</Typography>
         </Container>
         </Paper>
+    )
+}
+
+const CommentForm = () => {
+    
+}
+
+const fetchComments = async (post_id: number) => {
+    
+}
+
+type Comment = {
+    ID: number,
+    CreatedAt: string,
+
+
+}
+type CommentListProps = {
+    post_id: number,
+    CreatedAt: string,
+}
+const CommentList: React.FC<CommentListProps> = (props: CommentListProps) => {
+    const [comments, setComments] = useState([]);
+    const { post_id } = props
+    useEffect(() => {
+        Axios.get(`${API_ROOT}/${post_id}/comments`).then(
+            (res: AxiosResponse) => {setComments(res.data)}
+        )
+    }, [])
+    
+    return(
+        <>
+        
+
+        </>
     )
 }
 
