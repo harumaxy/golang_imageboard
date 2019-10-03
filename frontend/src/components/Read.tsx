35352487c04e0f0e444bec12912f7caf280c89cc
max <greatsword8821@gmail.com>
2019-10-03 19:17:55 +0900

    大画面のときは2カラムになるように変更

--- frontend/src/components/Read.tsx
+++ frontend/src/components/Read.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, ReactNode } from "react"
-import { Container, Grid, Typography, Hidden, Paper } from "@material-ui/core";
+import { Container, Grid, Typography, Hidden, Paper, Grow } from "@material-ui/core";
 import { PostContainer } from "../containers/PostContainer"
 import { string } from "prop-types";
 import Axios, { AxiosResponse } from "axios";
@@ -39,17 +39,24 @@ const Read = (props: any) => {
 
     return (
         <Paper>
-            <Container>
-                <img src={post.image_src} alt={post.title} width="100%" />
-                <Typography variant="h4" component="h1">{post.title}</Typography>
-                <Typography variant="subtitle1" component="h2">{post.author}</Typography>
-                <Typography variant="body1" component="p">{post.description}</Typography>
-                <Typography variant="srOnly">{post.image_src}</Typography>
-                <Typography variant="srOnly">{id}</Typography>
-            </Container>
-            <Container>
-                <CommentList post_id={id} />
-            </Container>
+            <br />
+            <Grid container>
+                <Grid item xs={12} md={6}>
+                    <Container>
+                        <img src={post.image_src} alt={post.title} width="100%" />
+                        <Typography variant="h4" component="h1">{post.title}</Typography>
+                        <Typography variant="subtitle1" component="h2">{post.author}</Typography>
+                        <Typography variant="body1" component="p">{post.description}</Typography>
+                        <Typography variant="srOnly">{post.image_src}</Typography>
+                        <Typography variant="srOnly">{id}</Typography>
+                    </Container>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <Container>
+                        <CommentList post_id={id} />
+                    </Container>
+                </Grid>
+            </Grid>
         </Paper>
     )
 }
@@ -87,26 +94,22 @@ const CommentList: React.FC<CommentListProps> = (props: CommentListProps) => {
 
     return (
         <>
-            <br/>
             <hr />
             <h3>Comments</h3>
             <div style={{ overflow: "scroll", height: 300, border: "solid 1px gray" }}>
 
                 {comments.map((com, index) => (
                     <>
-                    <Grid container alignItems="center" style={{margin: 10}}>
-                        <Grid item xs={6}>
-                            <Typography variant="h6">{com.ID}: {com.author}</Typography>
-                        </Grid>
-                        <Grid item xs={6}>
-                            <Typography variant="body2">{com.CreatedAt}</Typography>
-                        </Grid>
-                        <Grid item xs={12}>
+                        <Grid container style={{ margin: 10 }}>
+                            <Grid item xs={12} sm={6}>
+                                <Typography variant="h6">{com.ID}: {com.author}</Typography>
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <Typography variant="body2">{com.CreatedAt}</Typography>
+                            </Grid>
                             <p>{com.body}</p>
                         </Grid>
-                        
-                    </Grid>
-                    <hr/>
+                        <hr />
                     </>
                 ))}
 
