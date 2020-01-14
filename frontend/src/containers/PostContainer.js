18576bd0a8e9b7b149dc7f7d1fce5f9d0bd884f9
Masaharu Hosomichi <masaharuhosomichi@MasaharunoiMac.local>
2020-01-14 16:19:16 +0900

    urlにhttp://を追加

--- frontend/src/containers/PostContainer.js
+++ frontend/src/containers/PostContainer.js
@@ -1,27 +1,26 @@
-import { useState } from "react"
-import { createContainer } from "unstated-next"
-import axios from "axios"
-import _ from "lodash"
-import {API_ROOT} from '../setting'
+import { useState } from "react";
+import { createContainer } from "unstated-next";
+import axios from "axios";
+import _ from "lodash";
+import { API_ROOT } from "../setting";
 
+const usePostContainer = () => {
+  const [posts, setPosts] = useState({});
 
-const usePostContainer = ()=>{
-    const [posts, setPosts] = useState({})
+  const fetch_posts = async () => {
+    const res = await axios.get(`http://${API_ROOT}/posts`);
+    setPosts(_.mapKeys(res.data, "ID"));
+  };
 
-    const fetch_posts = async () => {
-        const res = await axios.get(`${API_ROOT}/posts`)
-        setPosts(_.mapKeys(res.data, "ID"))
-    }
+  const fetch_single_post = async postID => {
+    const res = await axios.get(`${API_ROOT}/posts/${postID}`);
+    let newPosts = {};
+    Object.assign(newPosts, posts);
+    newPosts[postID] = res.data;
+    setPosts(newPosts);
+  };
 
-    const fetch_single_post = async (postID) => {
-        const res = await axios.get(`${API_ROOT}/posts/${postID}`)
-        let newPosts = {}
-        Object.assign(newPosts, posts)
-        newPosts[postID] = res.data
-        setPosts(newPosts)
-    }
+  return { posts, fetch_posts, fetch_single_post };
+};
 
-    return {posts, fetch_posts, fetch_single_post}
-}
-
-export const PostContainer = createContainer(usePostContainer)
+export const PostContainer = createContainer(usePostContainer);
