e69110cec75200d4de1ed065bb11ba0dd5401ad9
Masaharu Hosomichi <masaharuhosomichi@MasaharunoiMac.local>
2020-01-19 22:54:47 +0900

    auth0のBackend APIのaudienceを追加

--- frontend/src/setting.ts
+++ frontend/src/setting.ts
@@ -3,3 +3,4 @@ export const PUBLIC_URL = "storage.googleapis.com/imageboard-frontpage8821"
 
 export const AUTH0_DOMAIN = "max-project.auth0.com"
 export const AUTH0_CLIENT_ID = "3B4y2bgrI3Jiz5QxVGGXMOxHspYHNQvp"
+export const AUTH0_AUDIENCE = "https://golang-imageboard-8821.tk"
