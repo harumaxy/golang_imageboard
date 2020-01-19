a8569e728df5af6dac5f658081a0816fcf8d5e3c
Masaharu Hosomichi <masaharuhosomichi@MasaharunoiMac.local>
2020-01-19 22:25:06 +0900

    Auth0Providerでアプリ全体をラップ

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom"
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom"
 import * as serviceWorker from "./serviceWorker"
+import "./index.css"
 
 import { PostContainer } from "./containers/PostContainer"
 
@@ -12,26 +13,36 @@ import Lock from "./components/AuthButton"
 import AppBar from "./components/AppBar"
 import SncakbarContainer from "./containers/SncakbarContainer"
 import { PUBLIC_URL } from "./setting"
+import { Auth0Provider } from "./containers/react-auth0-spa"
+import { AUTH0_CLIENT_ID, AUTH0_DOMAIN } from "./setting"
+import history from "./utils/history"
+
+// リダイレクトを使うとき、historyに目的のurlをpushする
+const onRedirectCallback = (appState: any) => {
+  history.push(appState && appState.targetUrl ? appState.targetUrl : window.location.pathname)
+}
 
 ReactDOM.render(
-  <PostContainer.Provider>
-    <SncakbarContainer.Provider>
-      <Router basename={PUBLIC_URL}>
-        <AppBar />
-        <Switch>
-          <Route exact path="/posts/create" component={Create} />
-          <Route exact path="/posts/:id" component={Read} />
-          <Route exact path="" component={List} />
-        </Switch>
-        <p>
-          <Link to="">List</Link>
-        </p>
-        <p>
-          <Link to="/posts/create">Create</Link>
-        </p>
-      </Router>
-    </SncakbarContainer.Provider>
-  </PostContainer.Provider>,
+  <Auth0Provider domain={AUTH0_DOMAIN} client_id={AUTH0_CLIENT_ID} redirect_uri={window.location.origin} onRedirectCallback={onRedirectCallback as any}>
+    <PostContainer.Provider>
+      <SncakbarContainer.Provider>
+        <Router basename={PUBLIC_URL}>
+          <AppBar />
+          <Switch>
+            <Route exact path="/posts/create" component={Create} />
+            <Route exact path="/posts/:id" component={Read} />
+            <Route exact path="" component={List} />
+          </Switch>
+          <p>
+            <Link to="">List</Link>
+          </p>
+          <p>
+            <Link to="/posts/create">Create</Link>
+          </p>
+        </Router>
+      </SncakbarContainer.Provider>
+    </PostContainer.Provider>
+  </Auth0Provider>,
 
   document.getElementById("root")
 )
