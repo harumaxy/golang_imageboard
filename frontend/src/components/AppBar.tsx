83c154e2d74a4f79649ce74d65c4e7c8b8213d90
Masaharu Hosomichi <masaharuhosomichi@MasaharunoiMac.local>
2020-01-20 20:33:41 +0900

    '投稿する'ボタンを追加

--- frontend/src/components/AppBar.tsx
+++ frontend/src/components/AppBar.tsx
@@ -1,20 +1,28 @@
 import React from "react"
-import { AppBar, Toolbar, Typography, Link } from "@material-ui/core"
-import { Link as RouterLink } from 'react-router-dom'
-import AuthButton from "./AuthButton";
+import { AppBar, Toolbar, Typography, Link, Grid } from "@material-ui/core"
+import { Link as RouterLink } from "react-router-dom"
+import AuthButton from "./AuthButton"
 
 const Appbar = () => {
-    return (
-        <AppBar position="sticky">
-            <Toolbar>
-                <Link to="/" component={RouterLink} color="inherit" variant="h6">
-                    Golang_Imageboard
-                </Link>
-                <span id="spacer" style={{flexGrow: 1}}/>
-                <AuthButton />
-            </Toolbar>
-        </AppBar>
-    )
+  return (
+    <AppBar position="sticky">
+      <Toolbar>
+        <Link to="/" component={RouterLink} color="inherit" variant="h6">
+          Golang_Imageboard
+        </Link>
+        <span id="spacer" style={{ flexGrow: 1 }} />
+        <Grid container direction="row" spacing={2} alignItems="center" justify="flex-end">
+          <Grid item>
+            <Link to="/posts/create" component={RouterLink} color="inherit" variant="h6">
+              投稿する
+            </Link>
+          </Grid>
+
+          <AuthButton />
+        </Grid>
+      </Toolbar>
+    </AppBar>
+  )
 }
 
 export default Appbar
