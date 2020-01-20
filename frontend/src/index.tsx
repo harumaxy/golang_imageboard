34f8b51f6ed48edbb04966fbaf0815240e67fb6d
Masaharu Hosomichi <masaharuhosomichi@MasaharunoiMac.local>
2020-01-20 20:37:49 +0900

    emotionでグローバルなcssスタイルを追加

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,7 +2,6 @@ import React from "react"
 import ReactDOM from "react-dom"
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom"
 import * as serviceWorker from "./serviceWorker"
-import "./index.css"
 
 import { PostContainer } from "./containers/PostContainer"
 
@@ -16,6 +15,11 @@ import { PUBLIC_URL, AUTH0_AUDIENCE } from "./setting"
 import { Auth0Provider } from "./containers/react-auth0-spa"
 import { AUTH0_CLIENT_ID, AUTH0_DOMAIN } from "./setting"
 import history from "./utils/history"
+import gopher from "./gopher.png"
+
+import { jsx, Global, css } from "@emotion/core"
+import emotionReset from "emotion-reset"
+// import "./index.css"
 
 // リダイレクトを使うとき、historyに目的のurlをpushする
 const onRedirectCallback = (appState: any) => {
@@ -23,32 +27,53 @@ const onRedirectCallback = (appState: any) => {
 }
 
 ReactDOM.render(
-  <Auth0Provider
-    domain={AUTH0_DOMAIN}
-    client_id={AUTH0_CLIENT_ID}
-    redirect_uri={window.location.origin}
-    onRedirectCallback={onRedirectCallback as any}
-    audience={AUTH0_AUDIENCE}
-  >
-    <PostContainer.Provider>
-      <SncakbarContainer.Provider>
-        <Router basename={PUBLIC_URL}>
-          <AppBar />
-          <Switch>
-            <Route exact path="/posts/create" component={Create} />
-            <Route exact path="/posts/:id" component={Read} />
-            <Route exact path="" component={List} />
-          </Switch>
-          <p>
-            <Link to="">List</Link>
-          </p>
-          <p>
-            <Link to="/posts/create">Create</Link>
-          </p>
-        </Router>
-      </SncakbarContainer.Provider>
-    </PostContainer.Provider>
-  </Auth0Provider>,
+  <>
+    <Global
+      styles={css`
+        ${emotionReset}
+
+        *, *::after, *::before {
+          box-sizing: border-box;
+          -moz-osx-font-smoothing: grayscale;
+          -webkit-font-smoothing: antialiased;
+          font-smoothing: antialiased;
+        }
+
+        body {
+          background-image: url(${gopher});
+          background-repeat: repeat;
+          background-size: 200px;
+          background-position: center;
+          filter: alpha(opacity=50);
+          height: 100vh;
+          background-color: rgba(255, 255, 255, 0.6);
+          background-blend-mode: lighten;
+        }
+      `}
+    />
+    <Auth0Provider
+      domain={AUTH0_DOMAIN}
+      client_id={AUTH0_CLIENT_ID}
+      redirect_uri={window.location.origin}
+      onRedirectCallback={onRedirectCallback as any}
+      audience={AUTH0_AUDIENCE}
+    >
+      <PostContainer.Provider>
+        <SncakbarContainer.Provider>
+          <Router basename={PUBLIC_URL}>
+            <AppBar />
+            <div style={{ margin: 10 }}>
+              <Switch>
+                <Route exact path="/posts/create" component={Create} />
+                <Route exact path="/posts/:id" component={Read} />
+                <Route exact path="" component={List} />
+              </Switch>
+            </div>
+          </Router>
+        </SncakbarContainer.Provider>
+      </PostContainer.Provider>
+    </Auth0Provider>
+  </>,
 
   document.getElementById("root")
 )
