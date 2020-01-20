d615dd1a0d88eb43bccd98390caab6027b03c821
Masaharu Hosomichi <masaharuhosomichi@MasaharunoiMac.local>
2020-01-20 21:45:53 +0900

    react-routerのbaseurlを変更

--- frontend/src/setting.ts
+++ frontend/src/setting.ts
@@ -1,5 +1,5 @@
 export const API_ROOT = "https://golang-imageboard-8821.tk"
-export const PUBLIC_URL = "storage.googleapis.com/imageboard-frontpage8821"
+export const PUBLIC_URL = "imageboard-frontpage8821/index.html"
 
 export const AUTH0_DOMAIN = "max-project.auth0.com"
 export const AUTH0_CLIENT_ID = "3B4y2bgrI3Jiz5QxVGGXMOxHspYHNQvp"
